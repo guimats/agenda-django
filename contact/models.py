fbadef2c7fc967ef3700de16b9879ee5b721e5c9
guimats <guilherme.mat.ds@gmail.com>
2024-05-23 14:54:56 -0300

    Primeiro models (tabela contact)

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.db import models  # type: ignore
+from django.utils import timezone  # type: ignore
 
-# Create your models here.
+# id (primary key) - criado automatico pelo django
+# fist_name (string), last_name (string), phone (string)
+# email (email), created_date(date), description (text)
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (imagem)
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
