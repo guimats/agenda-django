5154494bdc07bcd9fb1849dbdf18e20f80936e9f
guimats <guilherme.mat.ds@gmail.com>
2024-05-23 15:39:49 -0300

    Customizando as opções da área administrativa Django

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from django.contrib import admin  # type: ignore
+from contact.models import Contact
 
-# Register your models here.
+
+# Cadastrando o model 'Contact'
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    # mostra as colunas informadas no display dos contacts
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    # ordena pelo valor informado - sinal de negativo '-' torna descrescente
+    ordering = '-id',
+    # cria um filtro para a tabela exibida
+    # list_filter = 'created_date',
+    # cria um campo de busca que procura nos campos informados
+    search_fields = 'id', 'first_name', 'last_name',
+    # quantos cadastros serão exibidos por página
+    list_per_page = 10
+    # máximo de cadastro que pode ser exibidio no 'mostrar tudo'
+    list_max_show_all = 100
+    # torna o dado editavel diretamente pelo display
+    list_editable = 'first_name', 'last_name'
+    # seleciona qual campo ficara com o link para acesso do cadastro
+    list_display_links = 'phone',
