e2371df04159f0576d30dc4e2d7c6d8b4b579b7e
guimats <guilherme.mat.ds@gmail.com>
2024-05-28 11:34:48 -0300

    Criando formulário dinamico

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,18 +1,60 @@
-from django.shortcuts import render, get_object_or_404, redirect  # type:ignore
-# importando os contatos para exibir na tela
-from contact.models import Contact  # type:ignore
-from django.db.models import Q  # type:ignore
+from typing import Any, Dict
+
+from django import forms  # type: ignore
+from django.core.exceptions import ValidationError  # type: ignore
+from django.core.paginator import Paginator  # type: ignore
+from django.db.models import Q  # type: ignore
+from django.shortcuts import get_object_or_404, redirect, render  # type:ignore
+
+from contact.models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
 
 
 def create(request):
     if request.method == 'POST':
-        print(request.POST.get('first_name'))
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
-
+        'form': ContactForm()
     }
 
     return render(
-        request, 'contact/create.html',
+        request,
+        'contact/create.html',
         context
-        )
+    )
