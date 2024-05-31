df98e83c3984014ba48735d50b28f26a7b619d5c
guimats <guilherme.mat.ds@gmail.com>
2024-05-31 10:10:17 -0300

    Separando o formulário para um novo arquivo

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,40 +1,6 @@
-from typing import Any, Dict
+from django.shortcuts import render  # type:ignore
 
-from django import forms  # type: ignore
-from django.core.exceptions import ValidationError  # type: ignore
-from django.core.paginator import Paginator  # type: ignore
-from django.db.models import Q  # type: ignore
-from django.shortcuts import get_object_or_404, redirect, render  # type:ignore
-
-from contact.models import Contact
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone',
-        )
-
-    def clean(self):
-        # cleaned_data = self.cleaned_data
-
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
-
-        return super().clean()
+from contact.forms import ContactForm
 
 
 def create(request):
