697ab73ca9033029e39f7101ac7612597887e6b1
guimats <guilherme.mat.ds@gmail.com>
2024-06-11 13:41:11 -0300

    Validação de campos do formulário

--- contact/forms.py
+++ contact/forms.py
@@ -7,8 +7,8 @@ class ContactForm(forms.ModelForm):
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                    'class': 'classe-a classe-b',
-                    'placeholder': 'Escreva aqui'
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva aqui'
             }
         ),
         label='Primeiro nome',
@@ -39,21 +39,28 @@ class ContactForm(forms.ModelForm):
         # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
+        if first_name == last_name:
+            msg = (ValidationError(
+                'Primeiro nome não pode ser igual ao sobrenome')
             )
-        )
-        self.add_error(
-            'first_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
-        return super().clean()
+            return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+        return first_name
